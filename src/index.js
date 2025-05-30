13090c0aadeca59000fd45101ddc7e2998921d54
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-05-30 15:07:00 +0530

    test commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import app from "./app.js"
 
+app.get("/",(req,res)=>{
+    res.send("<h1>This is Task Master</h1>")
+})
+
 app.listen(process.env.PORT,()=>{
     console.log(`\nServer running at http://localhost:${process.env.PORT}\n`)   
 })
